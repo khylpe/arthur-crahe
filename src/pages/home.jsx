0d13045262e844e69add861bc412c173434919cf
Arthur CRAHÉ <crahe.arthur@gmail.com>
2023-12-02 00:20:31 +0100

    updated maintenance page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,18 +11,18 @@ const Home = (props) => {
        const { language } = props;
 
        return (
-                     <div className='flex flex-col items-center justify-center py-8 sm:py-0'>
-                            <div className='flex flex-col items-center justify-center'>
-                                   <img
-                                          draggable="false"
-                                          className="w-auto hidden sm:block h-36"
-                                          src={theme === 'light' ? blackLogo : whiteLogo}
-                                          alt="Arthur CRAHE logo"
-                                   />
-                                   <h1 className='text-5xl font-bold text-center mt-10'>Arthur CRAHE</h1>
-                                   <h2 className='text-2xl font-bold text-center mt-5'>CESI | {translations[language].home.alternance} | {translations[language].home.job}</h2>
-                                   <div className="mt-8">
-                                          <Socials></Socials>
+              <div className='flex flex-col items-center justify-center py-8 sm:py-0'>
+                     <div className='flex flex-col items-center justify-center'>
+                            <img
+                                   draggable="false"
+                                   className="w-auto hidden sm:block h-36"
+                                   src={theme === 'light' ? blackLogo : whiteLogo}
+                                   alt="Arthur CRAHE logo"
+                            />
+                            <h1 className='text-5xl font-bold text-center mt-10'>Arthur CRAHE</h1>
+                            <h2 className='text-2xl font-bold text-center mt-5'>CESI | {translations[language].home.alternance} | {translations[language].home.job}</h2>
+                            <div className="mt-8">
+                                   <Socials></Socials>
                             </div>
                      </div>
               </div>
